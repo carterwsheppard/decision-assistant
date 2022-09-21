05dc737da80c6c3f614f7a57e32b5f58b126e2bb
hoganrsherrow <hogan.r.sherrow@vanderbilt.edu>
2022-09-20 20:21:59 -0500

    Separated login and signup pages

--- client/src/components/LoginSignUpPortal/index.js
+++ client/src/components/LoginSignUpPortal/index.js
@@ -2,23 +2,43 @@ import React from "react";
 
 function Login() {
     return (
-        <div className="flex center">
-            <div>
-            
-            <form className="flex contact-form">
-            <h3 className="">Login or Sign Up!</h3>
-            <br></br>
+        <div className="flex-row space-around">
+            <div className="flex center">
                 <div>
-                <label>Username:</label>
-                <input className="form-input" type="text" placeholder="Username" ></input>
+                    <form className="flex contact-form">
+                    <h3 className="">Login</h3>
+                    <br></br>
+                        <div>
+                        <label>Username:</label>
+                        <input className="form-input" type="text" placeholder="Username" ></input>
+                        </div>
+                        <br></br>
+                        
+                        <label>Password:</label>
+                        <input className="form-input" type="text" placeholder="Password"></input>
+                        <br></br>
+                        <button onClick={() => console.log("Add Submit Logic")}>Submit</button>
+                    </form>
                 </div>
-                <br></br>
-                
-                <label>Password:</label>
-                <input className="form-input" type="text" placeholder="Password"></input>
-                <br></br>
-                <button onClick={() => console.log("Add Submit Logic")}>Submit</button>
-            </form>
+            </div>
+            <div className="center">OR</div>
+            <div className="flex center">
+                <div>
+                    <form className="flex contact-form">
+                    <h3 className="">Signup</h3>
+                    <br></br>
+                        <div>
+                        <label>Username:</label>
+                        <input className="form-input" type="text" placeholder="Username" ></input>
+                        </div>
+                        <br></br>
+                        
+                        <label>Password:</label>
+                        <input className="form-input" type="text" placeholder="Password"></input>
+                        <br></br>
+                        <button onClick={() => console.log("Add Submit Logic")}>Submit</button>
+                    </form>
+                </div>
             </div>
         </div>
     );
